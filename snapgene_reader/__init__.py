1553d079d47e82fb588ba164d942c08e9df6f93c
Isaac Luo <yishaluo@gmail.com>
2018-01-23 18:28:24 +0000

    pep8

--- snapgene_reader/__init__.py
+++ snapgene_reader/__init__.py
@@ -1,1 +1,7 @@
+'''
+snapgene_reader module
+usage:
+    from snapgene_reader import snapgene_file_to_dict
+    obj = snapgene_file_to_dict(file_path='test.dna')
+'''
 from .snapgene_reader import snapgene_file_to_dict, snapgene_file_to_seqrecord
