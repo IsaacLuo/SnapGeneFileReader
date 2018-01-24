d8880776ac8539801d9d939300cb1c529a9d85de
Zulko <valentin.zulkower@gmail.com>
2018-01-24 10:55:37 +0000

    Added snapgene_file_to_gbk to test suite. Seems broken.

--- snapgene_reader/__init__.py
+++ snapgene_reader/__init__.py
@@ -1,1 +1,3 @@
-from .snapgene_reader import snapgene_file_to_dict, snapgene_file_to_seqrecord
+from .snapgene_reader import (snapgene_file_to_dict,
+                              snapgene_file_to_seqrecord,
+                              snapgene_file_to_gbk)
--- tests/test_general.py
+++ tests/test_general.py
@@ -1,11 +1,11 @@
-from snapgene_reader import snapgene_file_to_seqrecord
+from snapgene_reader import snapgene_file_to_seqrecord, snapgene_file_to_gbk
 from Bio import SeqIO
 import os
 
 TEST_DIR = os.path.join('tests', 'test_samples')
 
 
-def test_parse(tmpdir):
+def test_snapgene_file_to_seqrecord(tmpdir):
     all_files = [f for f in os.listdir(TEST_DIR) if f.endswith('.dna')]
     assert len(all_files)
     for fname in all_files:
@@ -14,3 +14,15 @@ def test_parse(tmpdir):
         assert len(record.seq) > 10
         with open(os.path.join(str(tmpdir), fname + '.gb'), 'w') as f:
             SeqIO.write([record, ], f, 'genbank')
+
+def test_snapgene_file_to_gbk(tmpdir):
+    all_files = [f for f in os.listdir(TEST_DIR) if f.endswith('.dna')]
+    assert len(all_files)
+    for fname in all_files:
+        print (fname)
+        fpath = os.path.join(TEST_DIR, fname)
+        target = os.path.join(str(tmpdir), 'testfile.gbk')
+        with open(fpath, 'r') as fsource:
+            with open(target, 'w') as ftarget:
+                snapgene_file_to_gbk(fsource, ftarget)
+        verification_reparsing = SeqIO.read(target, 'genbank')
