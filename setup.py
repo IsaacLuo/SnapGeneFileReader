3df651768c02a2525179f64c12ca993604ff677b
Peter Vegh <pvegh@posteo.uk>
2020-09-05 12:46:02 +0100

    License autodetect fix

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-'''
+"""
 setup.py
 install snapgene_reader by pip
-'''
+"""
 from setuptools import setup, find_packages
 
-setup(name='snapgene_reader',
-      version='0.1.17',
-      author='yishaluo',
-      author_email='yishaluo@gmail.com',
-      maintainer='Zulko',
-      description='Convert Snapgene *.dna files dict/json/biopython.',
-      long_description=open('README.rst').read(),
-      license='MIT',
-      keywords="DNA sequence design format converter",
-      packages=find_packages(),
-      install_requires=['biopython', 'xmltodict', 'html2text'])
+setup(
+    name="snapgene_reader",
+    version="0.1.17",
+    author="yishaluo",
+    maintainer="EdinburghGenomeFoundry",
+    description="Convert Snapgene *.dna files dict/json/biopython.",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    keywords="DNA sequence design format converter",
+    packages=find_packages(),
+    install_requires=["biopython", "xmltodict", "html2text"],
+)
